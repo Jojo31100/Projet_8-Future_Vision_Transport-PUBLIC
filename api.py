22a315fef0a66eb770862ba9e4e906bc61748448
Jojo31100 <Joel@Caquelin.Fr>
2025-10-29 11:59:35 +0100

    Test de mise en place de la nouvelle API (support YOLO) pour voir si ca fonctionne a minima sans planter...

--- api.py
+++ api.py
@@ -1,15 +1,13 @@
-#API v1.0
+#API v1.1_alpha
 
 
 #Imports
 import os
 import numpy
 from PIL import Image
-from fastapi import FastAPI
 from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-import tensorflow
 import base64
 import io
 import tensorflow
@@ -19,13 +17,17 @@ from tensorflow.keras.models import Model
 from tensorflow.keras import backend
 from tensorflow.keras.models import load_model
 from fastapi.responses import JSONResponse
+from ultralytics import YOLO
+import cv2
 
 
 #Chargement du modèle
 cheminModele = "./model/best_model_VGG16Unet_sans_DataAugmentation.keras"
+cheminModele_YOLO = "./model/best_model_YOLOv8-l-seg_fine-tuning.pt"
 repertoireDonneesDeTest = "./testPictures/"
 modelCharge = load_model(cheminModele, compile=False)
-app = FastAPI(title="VGG16-Unet API")
+modelCharge_YOLO = YOLO(cheminModele_YOLO)
+app = FastAPI(title="VGG16-Unet & YOLOv8 API")
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
 #Classe pour la prédiction
@@ -48,15 +50,15 @@ def resizeHorizontalX2(image_array):
 #Endpoint racine
 @app.get("/")
 async def root():
-    return {"message": "API VGG16-Unet v1.0"}
+    return {"message": "API VGG16-Unet & YOLOv8 v1.1_alpha"}
 
-#Endpoint : Lister les fichiers de test
+#Endpoint VGG16+Unet & YOLO : Lister les fichiers de test
 @app.post("/list")
 def listeFichiers():
     fichiers = sorted([fichier for fichier in os.listdir(repertoireDonneesDeTest) if fichier.endswith("_leftImg8bit.png")])
     return {"Nombre de fichiers": len(fichiers), "fichiers": fichiers}
 
-#Endpoint : Prédire sur un numéro
+#Endpoint VGG16+Unet : Prédire sur un numéro
 @app.post("/predict")
 def predict(request: PredictionRequest):
     fichiersCamera = sorted([fichier for fichier in os.listdir(repertoireDonneesDeTest) if fichier.endswith("_leftImg8bit.png")])
@@ -75,7 +77,40 @@ def predict(request: PredictionRequest):
     masquePredit_array = numpy.argmax(prediction[0], axis=-1)
     return {"imageCamera": imageVersBase64(imageCamera_array), "masqueReel": imageVersBase64(imageMasque_array), "masquePredit": imageVersBase64(resizeHorizontalX2(masquePredit_array)), "numero_image": request.numero}
 
-#Endpoint : Uploader et prédire une image
+#Endpoint YOLO : Prédire sur un numéro
+@app.post("/predict_YOLO")
+def predict_YOLO(request: PredictionRequest):
+    fichiersCamera = sorted([fichier for fichier in os.listdir(repertoireDonneesDeTest) if fichier.endswith("_leftImg8bit.png")])
+    fichiersMasques = sorted([fichier for fichier in os.listdir(repertoireDonneesDeTest) if fichier.endswith("_gtFine_labelIds.png")])
+    index = request.numero
+    if((index < 0) or (index >= len(fichiersCamera))):
+        return {"error": "Numéro d'image invalide !"}
+    fichierCameraSelectionne = os.path.join(repertoireDonneesDeTest, fichiersCamera[index])
+    fichierMasqueSelectionne = os.path.join(repertoireDonneesDeTest, fichiersMasques[index])
+    imageCamera_array = numpy.array(Image.open(fichierCameraSelectionne))
+    imageMasque_array = numpy.array(Image.open(fichierMasqueSelectionne))
+    #Inférence YOLO
+    predictions = modelCharge_YOLO.predict(fichierCameraSelectionne, verbose=False)
+    #Récupération taille originale de l'image
+    height, width = imageMasque_array.shape[:2]
+    masquePreditFusionne_8 = numpy.zeros((height, width), dtype=int)
+    #YOLO travaille en segmentation par instance, et non pas segmentation par classe...
+    if((predictions[0].masks is not None) and (len(predictions[0].masks.data) > 0)):
+        for indexInstance, masqueInstance in enumerate(predictions[0].masks.data):
+            #Classe directement prédite par le modèle (déjà dans le système 0-7)
+            classe_8 = int(predictions[0].boxes.cls[indexInstance].cpu().numpy())
+            #Masque binaire 2D
+            masqueInstance_array = masqueInstance.cpu().numpy()
+            if(masqueInstance_array.ndim == 3):
+                masqueInstance_array = masqueInstance_array[0]  #Car parfois (1,Height,Width) au lieu de (Height,Width)
+            masqueInstance_bool = masqueInstance_array.astype(bool)
+            #Redimensionnement du masque d’instance pour correspondre à la taille du "masque réel"
+            masqueInstance_resized = cv2.resize(masqueInstance_bool.astype(numpy.uint8), (width, height), interpolation=cv2.INTER_NEAREST).astype(bool)
+            #Fusion de toutes les instances d’une même classe
+            masquePreditFusionne_8[masqueInstance_resized] = classe_8
+    return {"imageCamera": imageVersBase64(imageCamera_array), "masqueReel": imageVersBase64(imageMasque_array), "masquePredit": imageVersBase64(masquePreditFusionne_8), "numero_image": request.numero}
+
+#Endpoint VGG16+Unet : Uploader et prédire une image
 @app.post("/predict_upload")
 async def predict_upload(file: UploadFile = File(...)):
     try:
@@ -90,5 +125,35 @@ async def predict_upload(file: UploadFile = File(...)):
         prediction = modelCharge.predict(imagePrediction, verbose=0)
         masquePredit_array = numpy.argmax(prediction[0], axis=-1)
         return {"masquePredit": imageVersBase64(resizeHorizontalX2(masquePredit_array))}
+    except Exception as excpt:
+        return {"error": str(excpt)}
+
+@app.post("/predict_upload_YOLO")
+async def predict_upload_YOLO(file: UploadFile = File(...)):
+    try:
+        fichierOctets = await file.read()
+        if(len(fichierOctets) == 0):
+            return {"error": "Fichier vide ou non lu correctement"}
+        imageUploadee = Image.open(io.BytesIO(fichierOctets)).convert("RGB")
+        imageUploadee_array = numpy.array(imageUploadee)
+        height, width = imageUploadee_array.shape[:2]
+        #Inférence YOLO
+        predictions = modelCharge_YOLO.predict(numpy.array(imageUploadee), verbose=False)
+        masquePreditFusionne_8 = numpy.zeros((height, width), dtype=int)
+        #YOLO travaille en segmentation par instance, et non pas segmentation par classe...
+        if((predictions[0].masks is not None) and (len(predictions[0].masks.data) > 0)):
+            for indexInstance, masqueInstance in enumerate(predictions[0].masks.data):
+                #Classe directement prédite par le modèle (déjà dans le système 0-7)
+                classe_8 = int(predictions[0].boxes.cls[indexInstance].cpu().numpy())
+                #Masque binaire 2D
+                masqueInstance_array = masqueInstance.cpu().numpy()
+                if(masqueInstance_array.ndim == 3):
+                    masqueInstance_array = masqueInstance_array[0]  #Car parfois (1,Height,Width) au lieu de (Height,Width)
+                masqueInstance_bool = masqueInstance_array.astype(bool)
+                #Redimensionnement du masque d’instance pour correspondre à la taille du "masque réel"
+                masqueInstance_resized = cv2.resize(masqueInstance_bool.astype(numpy.uint8), (width, height), interpolation=cv2.INTER_NEAREST).astype(bool)
+                #Fusion de toutes les instances d’une même classe
+                masquePreditFusionne_8[masqueInstance_resized] = classe_8
+        return {"masquePredit": imageVersBase64(masquePreditFusionne_8)}
     except Exception as excpt:
         return {"error": str(excpt)}
